7dfba1e424b89ff5b3a40716e3db0cc9ae5df942
sarthi kalathiya <105535689+sarthi-kalathiya@users.noreply.github.com>
2022-07-09 15:45:05 +0530

    Add files via upload

---
+++ AC/17.cpp
@@ -0,0 +1,78 @@
+// created by @AM
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+typedef unsigned long long int ull;
+#define fastios                       \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL)
+#define rep(i, a, b) for (int(i) = (a); (i) < (b); ++(i))
+#define pb push_back
+#define mk make_pair
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define clr(x) memset(x, 0, sizeof(x))
+#define sortall(x) sort(all(x))
+#define sortrall(x) sort(rall(x))
+#define tr(it, x) for (auto it = x.begin(); it != x.end(); it++)
+#define trr(it, x) for (auto it = x.rbegin(); it != x.rend(); it++)
+#define deb(x) cout << #x << ": " << x << endl;
+#define deb2(x, y) cout << #x << ": " << x << " ~ " << #y << ": " << y << endl;
+#define deba(arr)            \
+    cout << #arr << " ~ [ "; \
+    for (auto n : arr)       \
+        cout << n << ' ';    \
+    cout << ']' << endl;
+#define debap(arr)                                 \
+    cout << #arr << " ~ [ ";                       \
+    for (auto n : arr)                             \
+        cout << n.first << '-' << n.second << ' '; \
+    cout << ']' << endl;
+#define nl "\n"
+const double PI = 3.1415926535;
+#define PI 3.141592653589793238462643383279502884L
+int N = 1;
+// datatypes
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define vi vector<int>
+#define vvi vector<vi>
+#define vpii vector<pii>
+#define vpll vector<pll>
+class Solution
+{
+public:
+    int search(vector<int> &nums, int target)
+    {
+        int n = nums.size();
+        int low = 0, high = n - 1;
+        while (low <= high)
+        {
+            int mid = (low + high) / 2;
+            if (nums[mid] == target)
+                return mid;
+            else if (nums[low] <= nums[mid])
+            {
+                if (target >= nums[low] && target < nums[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            }
+            else
+            {
+                if (target > nums[mid] && target <= nums[high])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+        }
+        return -1;
+    }
+};
+int32_t main()
+{
+    fastios;
+
+    return 0;
+}
