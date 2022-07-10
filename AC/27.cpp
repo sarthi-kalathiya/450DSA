4d231a658f3f36d052d91b617471e5363e895284
sarthi kalathiya <105535689+sarthi-kalathiya@users.noreply.github.com>
2022-07-10 22:22:02 +0530

    Update 27.cpp

--- AC/27.cpp
+++ AC/27.cpp
@@ -1,3 +1,5 @@
+//3sum
+
 class Solution
 {
 public:
@@ -14,17 +16,17 @@ public:
         {
             l = i + 1;
             r = n - 1;
-            int s = arr[i]*-1;
+            int s = arr[i]*-1; //we have to find for all pairs with this in rest of the array 
 
             while (l < r)
             {
                 if (arr[l] + arr[r] == s)
                 {
                     v[0] = arr[i];
-                    v[1] = arr[r--];
+                    v[1] = arr[r--]; //once a one pair is found for 2 sum there can be other pair as well 
                     v[2] = arr[l++];
                     sort(v.begin(), v.end());
-                    seet.insert(v);
+                    seet.insert(v); //we are making our result unique becauseif not then try it (0,0,0,0)
                 }
                 else if (arr[l] + arr[r] < s)
                     ++l;
@@ -32,7 +34,7 @@ public:
                     --r;
             }
         }
-        copy(seet.begin(),seet.end(),back_inserter(ans));
+        copy(seet.begin(),seet.end(),back_inserter(ans)); //back_inserter is necessary to copy via copy stl
         return ans;
     }
 };
